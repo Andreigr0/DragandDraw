e68c1e5d9ce97533cd70feac3790dece20705a1d
User <sevore@mail.ru>
2019-05-04 15:43:51 +0300

    Commit changes by Vlad

--- app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
@@ -31,6 +31,7 @@ class BoxDrawingView(context: Context, attrSet: AttributeSet? = null) : View(con
     }
 
     // Andrey comment
+    // Commit changes by Vlad
     private val drawingCallback: DrawingCallback = context as DrawingCallback
     var backgroundBitmap: Bitmap? = null
 
